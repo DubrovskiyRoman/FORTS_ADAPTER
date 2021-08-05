596d0a24bfb6fb39fc5fea46d243840bd6ed98e0
Roma-eTrade <roman@etrade.kz>
2021-08-05 17:52:03 +0600

    ver 0.1.2 add transaction support

--- src/main/java/kz/roma/adapter_forts/dao/messageTypeDao/MessageTypeDaoImpl.java
+++ src/main/java/kz/roma/adapter_forts/dao/messageTypeDao/MessageTypeDaoImpl.java
@@ -2,13 +2,16 @@ package kz.roma.adapter_forts.dao.messageTypeDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+
 public class MessageTypeDaoImpl implements MessageTypeDao {
     @Autowired
     MessageTypeRepo messageTypeRepo;
 
     @Override
+    @Transactional(readOnly = true)
     public long findMessageIdByName(String name) {
         return messageTypeRepo.findMessageTypeIdByName(name);
     }
